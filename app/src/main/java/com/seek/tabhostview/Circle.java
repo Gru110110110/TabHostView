7cab61d95a63edea2fa3669ff7a3e2d83d7d3385
seek <951882080@qq.com>
2016-08-02 14:10:32 +0800

    refresh~

---
+++ app/src/main/java/com/seek/tabhostview/Circle.java
@@ -0,0 +1,43 @@
+package com.seek.tabhostview;
+
+import android.graphics.Rect;
+
+public class Circle {
+    float mCenterX, mCenterY;
+    float mRadius;
+
+    Circle() {}
+
+    Circle(float centerX, float centerY, float radius) {
+        mCenterX = centerX;
+        mCenterY = centerY;
+        mRadius = radius;
+    }
+
+    boolean contains(float x, float y) {
+        x = mCenterX - x;
+        y = mCenterY - y;
+        return x * x + y * y <= mRadius * mRadius;
+    }
+
+    public float getCenterX() {
+        return mCenterX;
+    }
+
+    public float getCenterY() {
+        return mCenterY;
+    }
+
+    public float getRadius() {
+        return mRadius;
+    }
+
+    Rect getBoundingRect() {
+        return new Rect(Math.round(mCenterX - mRadius), Math.round(mCenterY - mRadius),
+                Math.round(mCenterX + mRadius), Math.round(mCenterY + mRadius));
+    }
+    @Override
+    public String toString() {
+        return "Radius: " + mRadius + " X: " + mCenterX +  " Y: " + mCenterY;
+    }
+}
